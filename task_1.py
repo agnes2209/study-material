d49b04b45aeb188e325568c9d792939af0889b3b
agnes2209 <agnedavi2209@gmail.com>
2025-06-04 15:14:18 +0530

    Initial commit

---
+++ task_1.py
@@ -0,0 +1,9 @@
+x=int(input("Enter value for x:"))
+y=int(input("Enter value for y:"))
+print("Before swapping:")
+print("x =", x)
+print("y =", y)
+x,y=y,x
+print("After swapping:")
+print("x =", x)
+print("y =", y)
